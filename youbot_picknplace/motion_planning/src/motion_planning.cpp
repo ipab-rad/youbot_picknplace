f83c837dc7b78f9d139244640e8556de5b4de4eb
Alex Silva <s1464657@sms.ed.ac.uk>
2015-07-02 01:41:25 +0100

    motion planning refactored - now sits on top of the hierarchy over motion, sensing - initial functionality of approaching an object using motion

--- youbot_picknplace/motion_planning/src/motion_planning.cpp
+++ youbot_picknplace/motion_planning/src/motion_planning.cpp
@@ -5,14 +5,14 @@
  * @date      2015-06-20
  */
 
- #include "motion_planning/motion_planner.hpp"
+ #include "motion_planning/plan_approach.hpp"
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "motion_planning");
   ros::NodeHandle nh("motion_planning");
   ROS_INFO("Running Motion Planning");
 
-  MotionPlanner motionplanner(&nh);
+  PlanApproachAction approachPlanner(nh, "plan_approach");
 
 
   ros::spin();
